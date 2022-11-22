ea87e04fdb99214e8960cf54e8389892f522619a
Najicablitz <david.ruizdeluzuriaga@ciit.edu.ph>
2022-11-22 22:21:20 +0800

    added sample Menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Load_Level()
+    {
+        
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Load_Menu()
+    {
+        
+        SceneManager.LoadScene("StartMenuScene");
+    }
+
+    public void Load_Post_Game()
+    {
+
+        SceneManager.LoadScene("Post-Game");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
